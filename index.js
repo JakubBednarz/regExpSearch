08ca04e17269f37e08792d79728a084bdbda84be
Jakub <j_bednarz28.03.92@wp.pl>
2020-05-17 14:12:18 +0200

    jest test fix

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const hashtagSearch = /(^|\s)(#[a-z\d-]+)/gi; //for testing with jest delete g flag
-const nameSearch = /(^|\s)([A-ZŚŁŻ][a-ząęółśżźćń]+)/g; //for testing with jest delete g flag
-const specialSignSearch = /(\s)([!@#$%^&*\-=+_?~])(\s)/g; //for testing with jest delete g flag
+const hashtagSearch = /(^|\s)(#[a-z\d-]+)/ig; 
+const nameSearch = /(^|\s)([A-ZŚŁŻ][a-ząęółśżźćń]+)/g; 
+const specialSignSearch = /(\s)([!@#$%^&*\-=+_?~])(\s)/g; 
 
-let tekst = "Lorem ipsum #net #lorem ipsum some#thing #js . lorem. # ipsum# ? why not?Andzia Łukasz Wandzia - Pomarańcza. Rzerzucha.bucha";
-console.log([...tekst.matchAll(hashtagSearch)]);
-console.log([...tekst.matchAll(nameSearch)]);
-console.log([...tekst.matchAll(specialSignSearch)]);
-
-
-console.log(nameSearch.test("Żaneta"));
+// let tekst = "Lorem ipsum #net #lorem ipsum some#thing #js . lorem. # ipsum# ? why not?Andzia Łukasz Wandzia - Pomarańcza. Rzerzucha.bucha";
+// console.log([...tekst.matchAll(hashtagSearch)]);
+// console.log([...tekst.matchAll(nameSearch)]);
+// console.log([...tekst.matchAll(specialSignSearch)]);
 
 module.exports = {
     hashtagSearch : hashtagSearch,
--- regExpSearch.test.js
+++ regExpSearch.test.js
@@ -1,27 +1,25 @@
 const {  hashtagSearch, nameSearch, specialSignSearch } = require("./index");
 
 test('Is hashtagSearch catching hashtag when it is first expresion in a string', () => {
-  expect(
-      hashtagSearch.test("#something")
-  ).toBeTruthy();
-});
+    expect('#lorem').toMatch(hashtagSearch);
+  });
 
 test('Is hashtagSearch catching hashtag when it is in middle of a sentence', () => {
     expect(
-        hashtagSearch.test("Aloha #haveANiceDay to every one")
+        hashtagSearch.test("Lorem #ipsum")
     ).toBeTruthy();
   });
 
 test('Is hashtagSearch not catching hashtag when it is in middle of a word', () => {
     expect(
-        hashtagSearch.test("Aloha today#haveANiceDay")
-    ).toBe(false);
+        hashtagSearch.test("Lorem lorem#ipsum")
+    ).toBeFalsy();
 });
 
 test('Is hashtagSearch not catching hashtag when it is just a hashtag with no letters', () => {
     expect(
-        hashtagSearch.test("Aloha # have a nice day")
-    ).toBe(false);
+        hashtagSearch.test("Lorem # ipsum")
+    ).toBeFalsy();
 });
 
 test('Is nameSearch catching name', () => {
@@ -33,29 +31,29 @@ test('Is nameSearch catching name', () => {
 test('Is nameSearch not catching name, when it is starting with lower case', () => {
     expect(
         nameSearch.test("jakub")
-    ).toBe(false);
+    ).toBeFalsy();
 });
 
 test('Is nameSearch not catching name, when it is in a middle of a word', () => {
     expect(
         nameSearch.test("jak tamJakubie?")
-    ).toBe(false);
+    ).toBeFalsy();
 });
 
 test('Is nameSearch catching name, when it has polish cases', () => {
     expect(
         nameSearch.test("Żaneta")
-    ).toBe(false);
+    ).toBeTruthy();
 });
 
 test('Is specialSignSearch catching special sign, when it is separetad with space', () => {
     expect(
-        specialSignSearch.test("Lorem . ipsum")
-    ).toBe(false);
+        specialSignSearch.test("Lorem ! ipsum")
+    ).toBeTruthy();
 });
 
 test('Is specialSignSearch not catching special sign, when it is not separetad with space', () => {
     expect(
-        specialSignSearch.test("Lorem. .ipsum")
-    ).toBe(false);
+        specialSignSearch.test("Lorem. ?ipsum!")
+    ).toBeFalsy();
 });
